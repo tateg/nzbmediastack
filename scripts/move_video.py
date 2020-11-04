93391d56124b14494bcc3b479f7ef41158267f68
Tate Galbraith <tateg@users.noreply.github.com>
2020-11-03 20:35:29 -0800

    Update move_video.py

--- scripts/move_video.py
+++ scripts/move_video.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import shutil
 import json
 import logging
 import requests
@@ -9,29 +10,41 @@ from termcolor import colored
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format="%(asctime)s — %(name)s — %(levelname)s — %(funcName)s:%(lineno)d — %(message)s"
+    format=(
+        "%(asctime)s — "
+        "%(name)s — "
+        "%(levelname)s — "
+        "%(funcName)s:%(lineno)d — "
+        "%(message)s"
+    )
 )
 
 LOGGER = logging.getLogger('CONSOLE')
-LOGGER.info(colored('Moby mover starting up...', 'green'))
-
-TVDB_API_KEY = "5e8485a16ce4ba020c142fa930adc285"
+TVDB_API_KEY = ""
 API_PARAM = f"?api_key={TVDB_API_KEY}"
 TVDB_BASE_URL = "https://api.themoviedb.org/3"
 OAK_TVID = "60603"
 EP_FILTERS = ['curse', 'oak', 'island']
+VALID_EXTENSIONS = ['mkv','avi','mpg','mp4']
+DEST_DIR = 'test_dir/The Curse Of Oak Island'
 
 def parse_response(res):
     raw = res.text
     return json.loads(raw)
 
-def get_tv_ep(id, season, ep):
-    url = f"{TVDB_BASE_URL}/tv/{id}/season/{str(season)}/episode/{str(ep)}{API_PARAM}"
-    LOGGER.info(colored(f"making request for: {url}", 'yellow'))
-    return parse_response(requests.get(url))
+def get_tv_ep(id, season, episode):
+    szn = str(season)
+    epz = str(episode)
+    url_base = f"{TVDB_BASE_URL}/tv/{id}"
+    url_season_w_ep = f"{url_base}/season/{szn}/episode/{epz}"
+    full_url = url_season_w_ep + API_PARAM
+    LOGGER.debug(colored(f"making request for: {full_url}", 'yellow'))
+    res = parse_response(requests.get(full_url))
+    LOGGER.debug(colored(res, 'yellow'))
+    return res
 
-def get_tv_ep_name(id, season, ep):
-    return get_tv_ep(id, season, ep)['name']
+def get_tv_ep_name(id, season, episode):
+    return get_tv_ep(id, season, episode)['name']
 
 def get_episodes_in_dir(filters):
     contents = os.listdir()
@@ -39,18 +52,54 @@ def get_episodes_in_dir(filters):
     for c in contents:
         lower_c = c.lower()
         if any(n in lower_c for n in filters):
-            eps.append(lower_c)
+            ep_files = os.listdir(c)
+            eps.append({
+                'original_dirname': c,
+                'lower_dirname': lower_c,
+                'contents': ep_files
+            })
     return eps
 
 def split_season_ep_ids(ep):
-    match = re.findall(r"(?:s|season)(\d{2})(?:e|x|episode|\n)(\d{2})", ep, re.I)
+    match = re.findall(
+        r"(?:s|season)(\d{2})(?:e|x|episode|\n)(\d{2})", ep, re.I
+    )
     return match[0]
 
+def get_vidfile(contents):
+    for item in contents:
+        extension = item.split('.')[1].lower()
+        if extension in VALID_EXTENSIONS:
+            return [item, extension]
+
+def move_vidfile(original, new, directory):
+    full_path = directory + new
+    LOGGER.info(colored(f"moving: {original} -> {full_path}", 'yellow'))
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    shutil.move(original, full_path)
+
+def cleanup_leftover_dir(dir):
+    LOGGER.info(colored(f"removing directory: {dir}", 'yellow'))
+    os.rmdir(dir)
+
+LOGGER.info(colored('--- moby mover starting up ---', 'green'))
+
+LOGGER.info(colored(f"getting episodes with filter: {EP_FILTERS}", 'green'))
 eps = get_episodes_in_dir(EP_FILTERS)
 
+LOGGER.info(colored('filtering episodes and preparing move', 'green'))
 for ep in eps:
-    split = split_season_ep_ids(ep)
-    ep_name = get_tv_ep_name(OAK_TVID, split[0], split[1])
-    LOGGER.info(colored(f"looked up: {ep_name}", 'yellow'))
-    ep_string = f"{ep_name} - S{split[0]}E{split[1]}.mkv"
-    LOGGER.info(colored(f"moving episode: {ep_string}", 'yellow'))
+    LOGGER.debug(colored(f"splitting: {ep.get('lower_dirname')}"))
+    split = split_season_ep_ids(ep.get('lower_dirname'))
+    s_num = split[0]
+    e_num = split[1]
+    ep_name = get_tv_ep_name(OAK_TVID, s_num, e_num)
+    LOGGER.info(colored(f"looked up: {ep_name} - S{s_num}E{e_num}", 'yellow'))
+    vidfile = get_vidfile(ep.get('contents'))
+    if vidfile is not None:
+        ep_string = f"{ep_name} - S{s_num}E{e_num}.{vidfile[1]}"
+        move_path = DEST_DIR + '/Season '+ s_num.lstrip('0') + '/'
+        file_to_move = ep.get('original_dirname') + '/' + vidfile[0]
+        move_vidfile(file_to_move, ep_string, move_path)
+    cleanup_leftover_dir(ep.get('original_dirname'))
